7e9ed3137658b7f41dadd0b06b1647c92e7ca6b8
Michelle Tilley <michelle@michelletilley.net>
2025-12-13 20:58:46 -0800

    Prepare for packaging

--- src/build.rs
+++ src/build.rs
@@ -5,11 +5,5 @@ mod render;
 mod search;
 mod source;
 
-pub use builder::{base_path_from_config, BuildError, BuildResult, Builder};
-pub use search::{build_search_index, SearchError};
-pub use document::{
-    parse_front_matter, ContentItem, Document, DocumentContent, FrontMatter, ParsedContent,
-    StaticFile,
-};
-pub use render::{NavLink, NavSection, PageContext, PageInfo, RenderError, Renderer, SiteContext};
-pub use source::{ResolvedSource, SourceError};
+pub use builder::{base_path_from_config, Builder};
+pub use search::build_search_index;
